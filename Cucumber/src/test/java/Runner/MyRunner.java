b3a4ce8287db9393a1a60cbff712feda387c92a2
dhirajahire0705 <dahire0705@gmail.com>
2026-02-07 18:19:24 +0530

    Update MyRunner.java

--- Cucumber/src/test/java/Runner/MyRunner.java
+++ Cucumber/src/test/java/Runner/MyRunner.java
@@ -1,21 +1,21 @@
 package Runner;
 
-
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@SuppressWarnings("deprecation")
-@CucumberOptions(features = "src/test/Feature", 
-                 glue = {"StepDefination"},
-                 tags = "@sanity",
-                 plugin = {"pretty","html:target/report.html" }
-
-		)
-
-
+@CucumberOptions(
+        features = "src/test/Feature",
+        glue = {"StepDefination"},
+        tags = "@sanity",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-report.html",
+                "json:target/cucumber.json"
+        },
+        monochrome = true
+)
 public class MyRunner {
-
 }
